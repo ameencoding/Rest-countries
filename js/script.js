a75360de67337519813a4b4b37555d53905ee860
ameencoding <ibnujokar@hotmail.com>
2024-08-22 05:19:35 -0700

    updated something

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 "use strict";
 
-import { data } from "./data.js";
+import { countries } from "./data.js";
 const container = document.querySelector(".wrapper");
 
-const renderCountry = function () {
+const btnFilter = document.querySelector(".filter span");
+const chevron = document.querySelector(".filter svg");
+const regionsContainer = document.querySelector(".regions");
+const regions = regionsContainer.querySelectorAll("ul li");
+
+const input = document.querySelector(".search-container input");
+
+const renderCountry = function (data) {
   container.innerHTML = data
     .map((d) => {
       return `<div class="card">
@@ -27,4 +34,36 @@ const renderCountry = function () {
     .join(" ");
 };
 
-renderCountry();
+input.addEventListener("input", function () {
+  const val = input.value.toLowerCase();
+  const searched = countries.filter(
+    (c) => c.name.toLowerCase().indexOf(val) !== -1
+  );
+
+  if (searched.length > 0) {
+    renderCountry(searched);
+  } else {
+    console.log("OOOPS");
+  }
+});
+
+btnFilter.addEventListener("click", function () {
+  regionsContainer.classList.toggle("hide");
+  chevron.classList.toggle("rotate");
+});
+
+regions.forEach((region) => {
+  region.addEventListener("click", function () {
+    chevron.classList.remove("rotate");
+    regionsContainer.classList.add("hide");
+    btnFilter.textContent = this.textContent;
+
+    let val = this.textContent;
+    if (this.textContent === "America") val = "Americas";
+
+    const byRegion = countries.filter((c) => c.region === val);
+    renderCountry(byRegion);
+  });
+});
+
+renderCountry(countries);
